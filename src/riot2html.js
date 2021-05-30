e35c76239bf07d8a9b9fd1b2efe79748de102e0f
Genki Katsutani <nibushibu@gmail.com>
2021-05-31 01:48:44 +0900

    first commit

---
+++ src/riot2html.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const mkdirp = require('mkdirp')
+const { render } = require('@riotjs/ssr')
+const register = require('@riotjs/ssr/register')
+const srcDirFromRoot = './src/html/pages'
+const outputDir = 'dist/'
+
+// Riot コンポーネントを require できるように
+register()
+
+glob(`${srcDirFromRoot}/**/*.riot`, (err, files) => {
+  if (err) return err
+  generateHtml(files)
+})
+
+const generateHtml = (files) => {
+  files.forEach((file) => {
+    const Root = require(`.${file}`).default
+    const html = render('html', Root)
+    const dir = path.join(
+      outputDir,
+      file.replace(srcDirFromRoot, '').replace(/riot$/, 'html')
+    )
+
+    mkdirp(path.parse(dir).dir).then(() => {
+      fs.writeFile(dir, html, (err) => {
+        if (err) throw err
+      })
+    })
+  })
+}
